dff5db888904fdf7bc68503a3c925f7f4e94143e
Purushothaman Shanmugam <purushothaman.shanmugam@ideas2it.com>
2024-02-08 22:09:25 +0530

    terraform file -04

--- main.tf
+++ main.tf
@@ -1,31 +1,65 @@
 terraform {
   required_providers {
-    aws = { 
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
-resource "aws_key_pair" "pem_key" {
-  key_name   = "pem-key"
-  public_key = tls_private_key.rsa.public_key_openssh
+provider "aws" {
+  region     = "AWS_REGION"
+  access_key = "AWS_ACCESS_KEY"
+  secret_key = "AWS_SECRET_KEY"
 }
 
-resource "tls_private_key" "rsa" {
+// To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "local" {
-  content  = tls_private_key.rsa.private_key_pem 
-  filename = "tfkey"
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}
+
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+# Create a security group
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "public_instance" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.pem_key.key_name
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
 
   tags = {
     Name = "public_instance"
@@ -35,10 +69,4 @@ resource "aws_instance" "public_instance" {
     volume_size = 30
     volume_type = "gp2"
   }
-
-  depends_on = [
-    aws_key_pair.pem_key,
-    local_file.local,
-  ]
 }
-
