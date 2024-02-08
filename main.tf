607d8298c49856373dac7beb9771fd12aafac42a
Purushothaman Shanmugam <purushothaman.shanmugam@ideas2it.com>
2024-02-08 22:20:29 +0530

    terraform file -07

--- main.tf
+++ main.tf
@@ -60,24 +60,4 @@ resource "aws_security_group" "sg_ec2" {
   }
 }
 
-resource "aws_instance" "public_instance" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
-
-  tags = {
-    Name = "public_instance"
-  }
-
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp2"
-  }
-
-  depends_on = [
-    aws_key_pair.key_pair,
-    aws_security_group.sg_ec2,
-  ]
-}
 
