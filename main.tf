51036097e32ebd338ace9e4c4f759e3fe6a16006
Purushothaman Shanmugam <purushothaman.shanmugam@ideas2it.com>
2024-02-08 18:21:04 +0530

    terraform file

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = { 
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "aws_key_pair" "pem_key" {
+  key_name   = "pem-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "foo" {
+  content  = tls_private_key.rsa.private_key_pem 
+  filename = "tfkey"
+}
+
+resource "aws_instance" "Purushoth_Terraform_Instance"{
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+
+ tags = {
+    Name = "public_instance"
+  }
+
+ root_block_device {
+   volume_size = 30
+   volume_type = "gp2"
+  }
+}
