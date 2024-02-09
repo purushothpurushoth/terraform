496a50f4dc5e95252bea9c40feff1e87b85229b3
Purushothaman Shanmugam <purushothaman.shanmugam@ideas2it.com>
2024-02-09 15:43:24 +0530

    sns error

--- notification.tf
+++ notification.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = { 
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 resource "aws_sns_topic" "terraform_sns_topic"{
     name = sns_topic_tf 
 }
