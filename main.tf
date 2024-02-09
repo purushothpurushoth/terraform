c377072aabcc432252564adba79945a3f5487c18
Purushothaman Shanmugam <purushothaman.shanmugam@ideas2it.com>
2024-02-09 14:07:37 +0530

    checkint the terraform script

--- main.tf
+++ main.tf
@@ -1,63 +1,25 @@
 terraform {
   required_providers {
-    aws = {
+    aws = { 
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+resource "aws_key_pair" "terraform_instance_key" {
+  key_name   = "terraform_key"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"  # Change this to your desired region
-}
-
-variable "key_name" {
-  description = "Name of the SSH key pair"
-  default     = "my-key"  # Change this to your desired key name
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"  # Change this to your desired instance type
-}
-
-variable "ami_id" {
-  description = "AMI ID for the desired image"
-  default     = "ami-0f5ee92e2d63afc18"  # Change this to your desired AMI
-}
-
-resource "tls_private_key" "rsa_4096" {
+resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
-  public_key = tls_private_key.rsa_4096.public_key_openssh
-}
-
-resource "aws_security_group" "sg_ec2" {
-  name        = "sg_ec2"
-  description = "Security group for EC2"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "local_file" "pem_folder" {
+  content  = tls_private_key.rsa.private_key_pem 
+  filename = "terraform_key"
 }
 
 
